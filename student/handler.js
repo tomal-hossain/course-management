b808dcf4854b07b10ed4fc570d3a7e603821b95c
tomal.enosisbd <tomal.hossain@enosisbd.com>
2020-10-23 19:56:34 +0600

    initial commit,  design project

---
+++ student/handler.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports.getAllCourses = async event => {
+
+};
+
+module.exports.getCourse = async event => {
+
+};
+
+module.exports.getInvokedCourses = async event => {
+
+};
+
+module.exports.invokeRequest = async event => {
+
+};
+
---
+++ teacher/handler.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports.uploadCourseFile = async event => {
+
+};
+
+module.exports.addCourse = async event => {
+
+};
+
+module.exports.getAllCourses = async event => {
+
+};
+
+module.exports.getCourse = async event => {
+
+};
+
+module.exports.editCourse = async event => {
+
+};
+
+module.exports.removeCourse = async event => {
+
+};
+
+module.exports.acceptInvokeRequest = async event => {
+
+};
+
+module.exports.declineInvokeRequest = async event => {
+
+};
